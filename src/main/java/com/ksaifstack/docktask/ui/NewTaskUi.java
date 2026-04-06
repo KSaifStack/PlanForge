43ab0189836b2569cc46b8813a99511df43d093d
Kareem <ksaifeldawlla@gmail.com>
2026-04-06 11:48:35 -0400

    Added create task button to newTaskUi.java

--- src/main/java/com/ksaifstack/docktask/ui/NewTaskUi.java
+++ src/main/java/com/ksaifstack/docktask/ui/NewTaskUi.java
@@ -5,16 +5,17 @@ import com.ksaifstack.docktask.util.AppTray;
 import javafx.application.Platform;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
+import static com.ksaifstack.docktask.ui.FontLoader.setFont;
+
 /*
 * Main Application Page
-*
-*
-* */
+*/
 public class NewTaskUi {
     private String username;
     private final StackPane rootStack = new StackPane();
@@ -40,9 +41,17 @@ public class NewTaskUi {
         //Calander Ui
         newCalendarUi calendar = new newCalendarUi(username);
         Pane calendarPane = calendar.getPane();
-        // Create Task,
-        // Welcome "user", live clock
-        // task-pane
+        // Create Task(Button),
+        Button createtaskb = new Button("+");
+        createtaskb.setFont(setFont(60));
+        createtaskb.setLayoutX(775);
+        createtaskb.setLayoutY(140.00);
+        createtaskb.setPrefWidth(150);
+        createtaskb.setPrefHeight(150);
+        pane.getChildren().add(createtaskb);
+        // Welcome "user"(Text)
+        // live clock(Method)
+        // task-pane(Class)
         //Ui Elements end here
         pane.getChildren().add(calendarPane);
         Platform.setImplicitExit(false);
